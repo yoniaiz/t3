68dff78e7b83a7ff6f0376cb2c5a2fd4d4ae2333
yoniaiz <yoni29396.gmail.com>
2023-04-10 20:34:20 +0300

    inital commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { type AppType } from "next/app";
+import { api } from "~/utils/api";
+
+import "~/styles/globals.css";
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return (
+    <ClerkProvider>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
+};
+
+export default api.withTRPC(MyApp);
